c3269103075896b1d44e9576a88d0ba5f547a1f2
Rich Haines <hello@richardhaines.dev>
2021-05-05 14:19:57 +0200

    schema changes

---
+++ prisma/migrations/20210430131002_init/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Invoice` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Invoice" DROP CONSTRAINT "Invoice_orderId_fkey";
+
+-- DropTable
+DROP TABLE "Invoice";
